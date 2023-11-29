6a21780d0e7556d25b9e8146f6fdbf00574f13aa
rumkugel13 <rumkugel13@outlook.com>
2023-11-29 12:46:43 +0100

    add day05

---
+++ day05.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"crypto/md5"
+	"encoding/hex"
+	"strconv"
+)
+
+func day05() {
+	var data = input05
+	var result string
+	var result2 = []byte("zzzzzzzz")
+
+	for i := 0; len(result) < 8 || strings.Contains(string(result2), "z"); i++ {
+		hash := md5.Sum([]byte(data + strconv.Itoa(i)))
+		str := hex.EncodeToString(hash[:])
+		if str[0:5] == "00000" {
+			if len(result) < 8 {
+				result += string(str[5])
+			}
+			index := str[5]
+			if index >= '0' && index <= '7' && result2[index - '0'] == 'z' {
+				result2[index - '0'] = str[6]
+			}
+		}
+	}
+
+	fmt.Println("Day 05 Part 1 Result: ", result)
+	fmt.Println("Day 05 Part 2 Result: ", string(result2))
+}
+
+var input05 = "ffykfhsq"
